b605dd9edd3447facc9958f6ac78222221ede59a
Marek Dvořák <marekdvorak@protonmail.com>
2023-05-17 14:59:50 +0200

    Final touch

--- OAuthWcfApp/Authorize/CorsModule.cs
+++ OAuthWcfApp/Authorize/CorsModule.cs
@@ -13,7 +13,7 @@ namespace OAuthWcfApp.Authorize
 
         public void Dispose()
         {
-            // Implementace Dispose() je volitelná.
+
         }
 
         private void OnBeginRequest(object sender, EventArgs e)
@@ -26,8 +26,6 @@ namespace OAuthWcfApp.Authorize
                 context.Response.Headers.Add("Access-Control-Allow-Origin", "*");
                 context.Response.Headers.Add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
                 context.Response.Headers.Add("Access-Control-Allow-Headers", "Content-Type");
-
-                // Zde můžete přidat další potřebné hlavičky CORS.
 
                 if (context.Request.HttpMethod == "OPTIONS")
                 {
